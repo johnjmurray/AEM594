bbc0e0dc92301e132b24af1c49e211f1ad27c49d
John Murray <j2murray12@gmail.com>
2019-11-22 08:14:18 -0500

    Monte Carlo to minimize MSE

--- pca.py
+++ pca.py
@@ -14,13 +14,16 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.pyplot import figure
 import numpy as np
 from numpy import polynomial
 from decimal import Decimal
+import statsmodels.api as sm
+from scipy import stats
 
 
-
+'''
 
 def genTrainTest(dFrame, percentTest):
     # Inputs: pd dataframe of input data, specify what percentage of set should be reserved for testing
@@ -29,7 +32,6 @@ def genTrainTest(dFrame, percentTest):
     trainFrame = dFrame[:splitPoint+1]
     testFrame = dFrame[splitPoint+1:]
     return trainFrame, testFrame
-
 
 def regress(depVar,indVar,degree):
     # Inputs: dependent variable array, independent variable array, desired degree of fit equation
@@ -41,7 +43,7 @@ def regress(depVar,indVar,degree):
         yy = np.array(regressionCoeffs[0][ii]*domain**ii)
         fitVals += yy
     return regressionCoeffs, fitVals, domain
-
+'''
 
 # PCA https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
 
@@ -56,25 +58,65 @@ pcArray = list(range(1,7))
 
 x = df.loc[:, features].values
 
-dfTrain, dfTest = train_test_split(df,test_size=0.4) # split data into 60/40 train/test
-dfTest, dfValidate = train_test_split(dfTest,test_size=0.5) # split test data 50/50 test/validate
-trainData = dfTrain.loc[:,features].values
-testData = dfTest.loc[:,features].values
-validationData = dfValidate.loc[:,features].values
+iter_n = 0
+min_mseTrain = min_mseTest = 1e10
 
-# Run PCA on training data
-pca = PCA()
-principalComponents = pca.fit_transform(trainData)
-principalDf = pd.DataFrame(data = principalComponents, columns = pcColumns)
+dfTrain = min_dfTrain
+dfTest = min_dfTest
+while iter_n < 100:
+    #dfTrain, dfTest = train_test_split(df,test_size=0.2) # split data into train/test
+    #dfTest, dfValidate = train_test_split(dfTest,test_size=0.5) # split test data 50/50 test/validate
+    trainData = dfTrain.loc[:,features].values
+    testData = dfTest.loc[:,features].values
+    #validationData = dfValidate.loc[:,features].values
+    
+    # Run PCA on training data
+    pca = PCA()
+    principalComponents = pca.fit_transform(trainData)
+    principalDf = pd.DataFrame(data = principalComponents, columns = pcColumns)
+    
+    transformedTestSet = pca.transform(testData)
+    dfTransformedTestSet = pd.DataFrame(data = transformedTestSet, columns = pcColumns)
+    
+    
+    regr = linear_model.LinearRegression()
+    y = dfTrain['Non-Secondary Structures Mass']
+    regr.fit(principalComponents,y)
+    dfRegrCoef = np.transpose(pd.DataFrame(regr.coef_))
+    
+    trainDataOnFitLine = regr.intercept_ + np.transpose(np.dot(dfRegrCoef,np.transpose(principalDf)))
+    testDataOnFitLine = regr.intercept_ + np.transpose(np.dot(dfRegrCoef,np.transpose(dfTransformedTestSet)))
+    residsTrain = list(dfTrain['Non-Secondary Structures Mass']) - trainDataOnFitLine
+    residsTest = list(dfTest['Non-Secondary Structures Mass']) - testDataOnFitLine
+    mseTrain = np.average(residsTrain**2)
+    mseTest = np.average(residsTest**2)
+    
+    if mseTrain < min_mseTrain and mseTest < min_mseTest:
+        min_mseTrain = mseTrain
+        min_mseTest = mseTest
+        min_dfTrain = dfTrain
+        min_dfTest = dfTest
+        minRegrCoef = dfRegrCoef
+        min_iter_n = iter_n
+    iter_n+=1
+    if iter_n%100 == 0:
+        print('%s mseTrain = %f , mseTest = %f'%(str(iter_n),mseTrain,mseTest))
+    
+print('%s mseTrain = %f , mseTest = %f'%(str(min_iter_n),min_mseTrain,min_mseTest))
+est = sm.OLS(y, principalComponents)
+est2 = est.fit()
+print(est2.summary())
 
-#Scree plot (nice-to-have)
-screeVec = pca.explained_variance_
-screeAx = figure().gca()
-plt.plot(pcArray,screeVec)
-screeAx.set_ylabel('Eigenvalues')
-screeAx.set_xlabel('Component Number')
-screeAx.set_title('Scree Plot')
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.scatter(list(principalDf['pc1']),list(principalDf['pc2']),list(dfTrain['Non-Secondary Structures Mass']))
+ax.scatter(list(dfTransformedTestSet['pc1']),list(dfTransformedTestSet['pc2']),list(dfTest['Non-Secondary Structures Mass']))
+pc1Domain = np.linspace(min(list(principalDf['pc1'])),max(list(principalDf['pc1'])),1000)
+pc2Domain = np.linspace(min(list(principalDf['pc2'])),max(list(principalDf['pc2'])),1000)
+fitLine = dfRegrCoef[0]*pc1Domain+dfRegrCoef[1]*pc2Domain
 
+ax.scatter(pc1Domain,pc2Domain,fitLine)
+'''
 # Multiple linear regression
 r2list = []
 r2listCv = []
@@ -96,18 +138,22 @@ for kk in range(1,7):
     mseList.append(mean_squared_error(y, y_c))
     mseListCv.append(mean_squared_error(y, y_cv))
     print(regr.intercept_,regr.coef_)
-
-transformedTestSet = pca.transform(testData)
-dfTransformedTestSet = pd.DataFrame(data = transformedTestSet, columns = pcColumns)
-
-dfRegrCoef = np.transpose(pd.DataFrame(regr.coef_))
+'''
 
 
-testDataOnFitLine = regr.intercept_ + np.transpose(np.dot(dfRegrCoef,np.transpose(dfTransformedTestSet)))
-resids = list(dfTest['Non-Secondary Structures Mass']) - testDataOnFitLine
-mse = np.average(resids**2)
-print(mse)
 
+### Plotting ###
+'''
+#Scree plot (nice-to-have)
+screeVec = pca.explained_variance_ratio_
+screeAx = figure().gca()
+plt.plot(pcArray,screeVec)
+screeAx.set_ylabel('Explained Variance Fraction')
+screeAx.set_xlabel('Component Number')
+screeAx.set_title('Scree Plot')
+'''
+'''
+# r-squared & mean squared error plots
 
 r2Ax = figure().gca()
 plt.plot(pcArray,r2list)
@@ -122,7 +168,7 @@ plt.plot(pcArray,mseListCv)
 mseAx.legend(['Calibration','Cross-Validation'])
 mseAx.set_xlabel('Component Number')
 mseAx.set_title('MSE')
-
+'''
 # BACKWARDS SELECTION??
 
 '''
