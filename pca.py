5093211c9543e0dbefc8d9cbdc09ce9e3f120218
John Murray <j2murray12@gmail.com>
2019-12-10 09:18:56 -0500

    Update pca.py

--- pca.py
+++ pca.py
@@ -17,13 +17,17 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.pyplot import figure
 import numpy as np
 import statsmodels.api as sm
+from sklearn.model_selection import cross_val_predict
+import scipy
+from sklearn.preprocessing import scale
 
 
 # PCA https://towardsdatascience.com/pca-using-python-scikit-learn-e653f8989e60
 
 # load dataset into Pandas DataFrame
-df = pd.read_csv('archive_structure_data.csv', names=['Study Name','Craft Name','Non-Secondary Structures Mass','Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume'])
+df = pd.read_csv('Data/Structures/archive_structure_data.csv', names=['Study Name','Craft Name','Non-Secondary Structures Mass','Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume'])
 features = ['Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume']
+
 
 pcColumns = ['pc1','pc2','pc3','pc4','pc5','pc6']
 pcArray = list(range(1,7))
@@ -32,17 +36,17 @@ pcArray = list(range(1,7))
 
 x = df.loc[:, features].values
 
+
+
 iter_n = 0
 min_mseTrain = min_mseTest = 1e10
 
-#dfTrain = min_dfTrain
-#dfTest = min_dfTest
+dfTrain = min_dfTrain
+dfTest = min_dfTest
 while iter_n < 100000:
     dfTrain, dfTest = train_test_split(df,test_size=0.2) # split data into train/test
-    #dfTest, dfValidate = train_test_split(dfTest,test_size=0.5) # split test data 50/50 test/validate
     trainData = dfTrain.loc[:,features].values
     testData = dfTest.loc[:,features].values
-    #validationData = dfValidate.loc[:,features].values
     
     # Run PCA on training data
     pca = PCA()
@@ -51,7 +55,7 @@ while iter_n < 100000:
     
     transformedTestSet = pca.transform(testData)
     dfTransformedTestSet = pd.DataFrame(data = transformedTestSet, columns = pcColumns)
-    
+    comps = pd.DataFrame(pca.components_, columns=pcColumns)
     
     regr = linear_model.LinearRegression()
     y = dfTrain['Non-Secondary Structures Mass']
@@ -65,7 +69,7 @@ while iter_n < 100000:
     mseTrain = np.average(residsTrain**2)
     mseTest = np.average(residsTest**2)
     
-    if mseTrain < min_mseTrain and mseTest < min_mseTest:
+    if mseTrain+mseTest < min_mseTrain+min_mseTest:
         min_mseTrain = mseTrain
         min_mseTest = mseTest
         min_dfTrain = dfTrain
@@ -77,7 +81,7 @@ while iter_n < 100000:
         print('%s mseTrain = %f , mseTest = %f'%(str(iter_n),mseTrain,mseTest))
     
 print('%s mseTrain = %f , mseTest = %f'%(str(min_iter_n),min_mseTrain,min_mseTest))
-est = sm.OLS(y, principalComponents)
+est = sm.OLS(min_dfTrain['Non-Secondary Structures Mass'], principalComponents)
 est2 = est.fit()
 print(est2.summary())
 
@@ -88,7 +92,8 @@ ax.scatter(list(dfTransformedTestSet['pc1']),list(dfTransformedTestSet['pc2']),l
 pc1Domain = np.linspace(min(list(principalDf['pc1'])),max(list(principalDf['pc1'])),1000)
 pc2Domain = np.linspace(min(list(principalDf['pc2'])),max(list(principalDf['pc2'])),1000)
 fitLine = regr.intercept_+dfRegrCoef[0][0]*pc1Domain+dfRegrCoef[1][0]*pc2Domain
-ax.scatter(pc2Domain,pc1Domain,fitLine)
+ax.scatter(pc1Domain,pc2Domain,fitLine)
+
 '''
 # Multiple linear regression
 r2list = []
@@ -114,14 +119,31 @@ for kk in range(1,7):
 '''
 
 
+### histogram plots of original data
+
+fig, axs = plt.subplots(nrows=3, ncols=2, constrained_layout=False)
+for pltCount in range(6):
+    print(pltCount)
+    print(axs.flat[pltCount])
+    print(list(df[features[pltCount]]))
+    axs.flat[pltCount].hist(x=np.array(df[features[pltCount]]), color='#0504aa',alpha=0.7, rwidth=0.85)
+    axs.flat[pltCount].set_title(features[pltCount])
+fig.tight_layout()
+plt.show()
+
+
+### Pearson Correlation Coefficients between original variables
+dfCorrs = pd.DataFrame(index=features,columns=features)
+for ft1 in features:
+    for ft2 in features:
+        dfCorrs[ft1][ft2] = scipy.stats.pearsonr(scale(df[ft1]),scale(df[ft2]))[0]
+
 
 ### Plotting ###
-'''
+
 #Scree plot (nice-to-have)
 screeVec = pca.explained_variance_ratio_
 screeAx = figure().gca()
-plt.plot(pcArray,screeVec)
+plt.plot(pcArray,screeVec,'o--')
 screeAx.set_ylabel('Explained Variance Fraction')
 screeAx.set_xlabel('Component Number')
-screeAx.set_title('Scree Plot')
-'''
