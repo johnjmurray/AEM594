0a0b611fdb7dfba905cc95f1b5da80c363fbc006
John Murray <j2murray12@gmail.com>
2019-12-17 06:46:45 -0500

    Update pca.py

--- pca.py
+++ pca.py
@@ -43,6 +43,7 @@ min_mseTrain = min_mseTest = 1e10
 
 dfTrain = min_dfTrain
 dfTest = min_dfTest
+min_transformedTrainSet = True
 while iter_n < 100000:
     dfTrain, dfTest = train_test_split(df,test_size=0.2) # split data into train/test
     trainData = dfTrain.loc[:,features].values
@@ -50,22 +51,34 @@ while iter_n < 100000:
     
     # Run PCA on training data
     pca = PCA()
-    principalComponents = pca.fit_transform(trainData)
-    principalDf = pd.DataFrame(data = principalComponents, columns = pcColumns)
+    transformedTrainSet = pca.fit_transform(trainData)
+    dfTransformedTrainSet = pd.DataFrame(data = transformedTrainSet, columns = pcColumns)
     
     transformedTestSet = pca.transform(testData)
     dfTransformedTestSet = pd.DataFrame(data = transformedTestSet, columns = pcColumns)
-    comps = pd.DataFrame(pca.components_, columns=pcColumns)
+    loadings = pd.DataFrame(pca.components_*np.sqrt(pca.explained_variance_), columns=pcColumns)
     
     regr = linear_model.LinearRegression()
     y = dfTrain['Non-Secondary Structures Mass']
-    regr.fit(principalComponents,y)
-    dfRegrCoef = np.transpose(pd.DataFrame(regr.coef_))
+    regr.fit(transformedTrainSet,y)
+    dfRegrCoef = pd.DataFrame(regr.coef_)
     
-    trainDataOnFitLine = regr.intercept_ + np.transpose(np.dot(dfRegrCoef,np.transpose(principalDf)))
-    testDataOnFitLine = regr.intercept_ + np.transpose(np.dot(dfRegrCoef,np.transpose(dfTransformedTestSet)))
-    residsTrain = list(dfTrain['Non-Secondary Structures Mass']) - trainDataOnFitLine
-    residsTest = list(dfTest['Non-Secondary Structures Mass']) - testDataOnFitLine
+    trainDataOnFitLine = []
+    for ii in range(len(dfTransformedTrainSet)):
+        subElement = 0
+        for jj in range(len(dfRegrCoef)):
+            subElement += dfRegrCoef.values[jj][0]*dfTransformedTrainSet.values[ii][jj]
+        trainDataOnFitLine.append(subElement+regr.intercept_)
+            
+    testDataOnFitLine = []
+    for ii in range(len(dfTransformedTestSet)):
+        subElement = 0
+        for jj in range(len(dfRegrCoef)):
+            subElement += dfRegrCoef.values[jj][0]*dfTransformedTestSet.values[ii][jj]
+        testDataOnFitLine.append(subElement+regr.intercept_)
+            
+    residsTrain = np.subtract(np.array(dfTrain['Non-Secondary Structures Mass']),trainDataOnFitLine)
+    residsTest = np.subtract(np.array(dfTest['Non-Secondary Structures Mass']),testDataOnFitLine)
     mseTrain = np.average(residsTrain**2)
     mseTest = np.average(residsTest**2)
     
@@ -76,24 +89,25 @@ while iter_n < 100000:
         min_dfTest = dfTest
         minRegrCoef = dfRegrCoef
         min_iter_n = iter_n
+        min_transformedTrainSet = transformedTrainSet
     iter_n+=1
     if iter_n%100 == 0:
         print('%s mseTrain = %f , mseTest = %f'%(str(iter_n),mseTrain,mseTest))
     
 print('%s mseTrain = %f , mseTest = %f'%(str(min_iter_n),min_mseTrain,min_mseTest))
-est = sm.OLS(min_dfTrain['Non-Secondary Structures Mass'], principalComponents)
+est = sm.OLS(min_dfTrain['Non-Secondary Structures Mass'], min_transformedTrainSet)
 est2 = est.fit()
 print(est2.summary())
-
+'''
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.scatter(list(principalDf['pc1']),list(principalDf['pc2']),list(dfTrain['Non-Secondary Structures Mass']))
+ax.scatter(list(dfTransformedTrainSet['pc1']),list(dfTransformedTrainSet['pc2']),list(dfTrain['Non-Secondary Structures Mass']))
 ax.scatter(list(dfTransformedTestSet['pc1']),list(dfTransformedTestSet['pc2']),list(dfTest['Non-Secondary Structures Mass']))
-pc1Domain = np.linspace(min(list(principalDf['pc1'])),max(list(principalDf['pc1'])),1000)
-pc2Domain = np.linspace(min(list(principalDf['pc2'])),max(list(principalDf['pc2'])),1000)
+pc1Domain = np.linspace(min(list(dfTransformedTrainSet['pc1'])),max(list(dfTransformedTrainSet['pc1'])),1000)
+pc2Domain = np.linspace(min(list(dfTransformedTrainSet['pc2'])),max(list(dfTransformedTrainSet['pc2'])),1000)
 fitLine = regr.intercept_+dfRegrCoef[0][0]*pc1Domain+dfRegrCoef[1][0]*pc2Domain
 ax.scatter(pc1Domain,pc2Domain,fitLine)
-
+'''
 '''
 # Multiple linear regression
 r2list = []
@@ -102,14 +116,14 @@ mseList = []
 mseListCv = []
 for kk in range(1,7):        
     pca = PCA(n_components=kk)
-    principalComponents = pca.fit_transform(trainData)
-    principalDf = pd.DataFrame(data = principalComponents, columns = pcColumns[:kk])
+    transformedTrainSet = pca.fit_transform(trainData)
+    dfTransformedTrainSet = pd.DataFrame(data = transformedTrainSet, columns = pcColumns[:kk])
     
     regr = linear_model.LinearRegression()
     y = dfTrain['Non-Secondary Structures Mass']
-    regr.fit(principalComponents,y)
-    y_c = regr.predict(principalComponents)
-    y_cv = cross_val_predict(regr,principalComponents,y,cv=10)
+    regr.fit(transformedTrainSet,y)
+    y_c = regr.predict(transformedTrainSet)
+    y_cv = cross_val_predict(regr,transformedTrainSet,y,cv=10)
     r2list.append(r2_score(y, y_c))
     r2listCv.append(r2_score(y, y_cv))
     # Calculate mean square error for calibration and cross validation
@@ -120,7 +134,7 @@ for kk in range(1,7):
 
 
 ### histogram plots of original data
-
+'''
 fig, axs = plt.subplots(nrows=3, ncols=2, constrained_layout=False)
 for pltCount in range(6):
     print(pltCount)
@@ -130,7 +144,7 @@ for pltCount in range(6):
     axs.flat[pltCount].set_title(features[pltCount])
 fig.tight_layout()
 plt.show()
-
+'''
 
 ### Pearson Correlation Coefficients between original variables
 dfCorrs = pd.DataFrame(index=features,columns=features)
@@ -140,10 +154,11 @@ for ft1 in features:
 
 
 ### Plotting ###
-
+'''
 #Scree plot (nice-to-have)
 screeVec = pca.explained_variance_ratio_
 screeAx = figure().gca()
 plt.plot(pcArray,screeVec,'o--')
 screeAx.set_ylabel('Explained Variance Fraction')
 screeAx.set_xlabel('Component Number')
+'''
