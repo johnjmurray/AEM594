68667517884cd26a9615539ec7c32a7b3360ba33
John Murray <j2murray12@gmail.com>
2020-03-22 10:32:19 -0400

    address jakub's comments

--- pca.py
+++ pca.py
@@ -1,120 +1,123 @@
 # -*- coding: utf-8 -*-
 """
-PCR On 
+Principal component regression for structural mass predictions of spacecraft
 @author: John Murray, The University of Alabama
 """
 
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.pyplot import figure
 import numpy as np
 import statsmodels.api as sm
-from sklearn.model_selection import cross_val_predict
 import scipy
 from sklearn.preprocessing import scale
 
 
-# PCA 
-
-# load dataset into Pandas DataFrame
-df = pd.read_csv('archive_structure_data_cleaned.csv', names=['Study Name','Craft Name','Non-Secondary Structures Mass','Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume'])
+"""
+PCA 
+load dataset into Pandas DataFrame
+"""
+df = pd.read_csv('data/archive_structure_data_cleaned.csv', names=['Study Name','Craft Name','Non-Secondary Structures Mass','Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume'])
 features = ['Fuel/Oxidizer Tanks Mass','Total Dry Mass','Total Wet Mass','Prop Mass','Surface Area','Volume']
 
 
-pcColumns = ['pc1','pc2','pc3','pc4','pc5','pc6']
-pcArray = list(range(1,7))
+pc_columns = ['pc1','pc2','pc3','pc4','pc5','pc6']
+pc_array = list(range(1,7))
 
-# Separating out the features from the Non-Secondary Structures Mass
+"""Separating out the features from the Non-Secondary Structures Mass"""
 
 x = df.loc[:, features].values
 
 
-# Monte Carlo analysis
-# Split data set randomly into training set and test set, run PCA on training set, transform test set using PCs, run linear regression on training set (Non-secondary Structural Mass vs. PCs), obtain regression coefficients, generate line of best fit, apply to transformed test data & generate predicted value for Non-secondary Structural Mass, find mean squared error between predicted and actual Non-Secondary Structural Mass for both training and testing data points. If sum of training + testing MSEs is less than the minimum sum yet found, record this split and retain it. If the sum is greater than the current "winner" split, do nothing. Loop for an arbitrary number of iterations. To cover all possible test/train split combinations would require 17! iterations. 
+"""
+Monte Carlo analysis
+Split data set randomly into training set and test set, run PCA on training set, transform test set using PCs, run linear regression on training set (Non-secondary Structural Mass vs. PCs), obtain regression coefficients, generate line of best fit, apply to transformed test data & generate predicted value for Non-secondary Structural Mass, find mean squared error between predicted and actual Non-Secondary Structural Mass for both training and testing data points. If sum of training + testing MSEs is less than the minimum sum yet found, record this split and retain it. If the sum is greater than the current "winner" split, do nothing. Loop for an arbitrary number of iterations. To cover all possible test/train split combinations would require 17! iterations.
+"""
 
 iter_n = 0
-min_mseTrain = min_mseTest = 1e10
+min_mse_train = min_mse_test = 1e10
 
-#dfTrain = min_dfTrain
-#dfTest = min_dfTest
-min_transformedTrainSet = True
+#df_train = min_df_train
+#df_test = min_df_test
+min_transformed_train_set = True
+
 while iter_n < 100000:
-    dfTrain, dfTest = train_test_split(df,test_size=0.2) # split data into train/test
-    trainData = dfTrain.loc[:,features].values
-    testData = dfTest.loc[:,features].values
+    df_train, df_test = train_test_split(df,test_size=0.2) # split data into train/test
+    train_data = df_train.loc[:,features].values
+    test_data = df_test.loc[:,features].values
     
-    # Run PCA on training data
+    """Run PCA on training data"""
     pca = PCA()
-    transformedTrainSet = pca.fit_transform(trainData)
-    dfTransformedTrainSet = pd.DataFrame(data = transformedTrainSet, columns = pcColumns)
+    transformed_train_set = pca.fit_transform(train_data)
+    df_transformed_train_set = pd.DataFrame(data = transformed_train_set, columns = pc_columns)
     
-    # Transform test set
-    transformedTestSet = pca.transform(testData)
-    dfTransformedTestSet = pd.DataFrame(data = transformedTestSet, columns = pcColumns)
-    loadings = pd.DataFrame(pca.components_*np.sqrt(pca.explained_variance_), columns=pcColumns)
+    """Transform test set"""
+    transformed_test_set = pca.transform(test_data)
+    df_transformed_test_set = pd.DataFrame(data = transformed_test_set, columns = pc_columns)
+    loadings = pd.DataFrame(pca.components_*np.sqrt(pca.explained_variance_), columns=pc_columns)
     
-    # Run linear regression (Non-Secondary Structural Mass vs. training PCs)
+    """Run linear regression (Non-Secondary Structural Mass vs. training PCs)"""
     regr = linear_model.LinearRegression()
-    y = dfTrain['Non-Secondary Structures Mass']
-    regr.fit(transformedTrainSet,y)
-    dfRegrCoef = pd.DataFrame(regr.coef_)
+    y = df_train['Non-Secondary Structures Mass']
+    regr.fit(transformed_train_set,y)
+    df_regr_coef = pd.DataFrame(regr.coef_)
     
-    # Generate list of predicted values for training data Non-Secondary Structural Mass, using the regression coefficients from above
-    trainDataOnFitLine = []
-    for ii in range(len(dfTransformedTrainSet)): # for each spacecraft element
-        subElement = 0
-        # multiply its value on the PC1 axis by the 1st regression coefficient, its value on the PC2 axis by the 2nd regression coefficient, etc., then sum and add the intercept value to generate predicted Non-Secondary Structural Mass
-        for jj in range(len(dfRegrCoef)):
-            subElement += dfRegrCoef.values[jj][0]*dfTransformedTrainSet.values[ii][jj]
-        trainDataOnFitLine.append(subElement+regr.intercept_)
-    # same for test data      
-    testDataOnFitLine = []
-    for ii in range(len(dfTransformedTestSet)):
-        subElement = 0
-        for jj in range(len(dfRegrCoef)):
-            subElement += dfRegrCoef.values[jj][0]*dfTransformedTestSet.values[ii][jj]
-        testDataOnFitLine.append(subElement+regr.intercept_)
+    """Generate list of predicted values for training data Non-Secondary Structural Mass, using the regression coefficients from above"""
+    train_data_on_fit_line = []
+    for ii in range(len(df_transformed_train_set)): # for each spacecraft element
+        sub_element = 0
+        """Multiply its value on the PC1 axis by the 1st regression coefficient, its value on the PC2 axis by the 2nd regression coefficient, etc., then sum and add the intercept value to generate predicted Non-Secondary Structural Mass"""
+        for jj in range(len(df_regr_coef)):
+            sub_element += df_regr_coef.values[jj][0]*df_transformed_train_set.values[ii][jj]
+        train_data_on_fit_line.append(sub_element+regr.intercept_)
     
-    # subtract predicted mass values from recorded mass values, square the result, and find the mean of this squared error across the sets
-    residsTrain = np.subtract(np.array(dfTrain['Non-Secondary Structures Mass']),trainDataOnFitLine)
-    residsTest = np.subtract(np.array(dfTest['Non-Secondary Structures Mass']),testDataOnFitLine)
-    mseTrain = np.average(residsTrain**2)
-    mseTest = np.average(residsTest**2)
+    """Same for test data"""
+    test_data_on_fit_line = []
+    for ii in range(len(df_transformed_test_set)):
+        sub_element = 0
+        for jj in range(len(df_regr_coef)):
+            sub_element += df_regr_coef.values[jj][0]*df_transformed_test_set.values[ii][jj]
+        test_data_on_fit_line.append(sub_element+regr.intercept_)
     
-    # if the sum of the two MSEs is less than anything previously found, hold over all the information from this iteration.
-    if mseTrain+mseTest < min_mseTrain+min_mseTest:
-        min_mseTrain = mseTrain
-        min_mseTest = mseTest
-        min_dfTrain = dfTrain
-        min_dfTest = dfTest
-        minRegrCoef = dfRegrCoef
+    """Subtract predicted mass values from recorded mass values, square the result, and find the mean of this squared error across the sets"""
+    resids_train = np.subtract(np.array(df_train['Non-Secondary Structures Mass']),train_data_on_fit_line)
+    resids_test = np.subtract(np.array(df_test['Non-Secondary Structures Mass']),test_data_on_fit_line)
+    mse_train = np.average(resids_train**2)
+    mse_test = np.average(resids_test**2)
+    
+    """If the sum of the two MSEs is less than anything previously found, hold over all the information from this iteration"""
+    if mse_train+mse_test < min_mse_train+min_mse_test:
+        min_mse_train = mse_train
+        min_mse_test = mse_test
+        min_df_train = df_train
+        min_df_test = df_test
+        minRegrCoef = df_regr_coef
         min_iter_n = iter_n
-        min_transformedTrainSet = transformedTrainSet
+        min_transformed_train_set = transformed_train_set
     iter_n+=1
     if iter_n%100 == 0:
-        print('%s mseTrain = %f , mseTest = %f'%(str(iter_n),mseTrain,mseTest))
+        print('%s mse_train = %f , mse_test = %f'%(str(iter_n),mse_train,mse_test))
     
-print('%s mseTrain = %f , mseTest = %f'%(str(min_iter_n),min_mseTrain,min_mseTest))
-est = sm.OLS(min_dfTrain['Non-Secondary Structures Mass'], min_transformedTrainSet)
+print('%s mse_train = %f , mse_test = %f'%(str(min_iter_n),min_mse_train,min_mse_test))
+est = sm.OLS(min_df_train['Non-Secondary Structures Mass'], min_transformed_train_set)
 est2 = est.fit()
 print(est2.summary())
 
-### Pearson Correlation Coefficients between original variables
+"""Pearson Correlation Coefficients between original variables"""
 dfCorrs = pd.DataFrame(index=features,columns=features)
 for ft1 in features:
     for ft2 in features:
         dfCorrs[ft1][ft2] = scipy.stats.pearsonr(scale(df[ft1]),scale(df[ft2]))[0]
 
-
-### Plotting ###
-#Scree plot (nice-to-have)
-screeVec = pca.explained_variance_ratio_
-screeAx = figure().gca()
-plt.plot(pcArray,screeVec,'o--')
-screeAx.set_ylabel('Explained Variance Fraction')
-screeAx.set_xlabel('Component Number')
+"""
+Plotting
+Scree plot (nice-to-have)
+"""
+scree_vec = pca.explained_variance_ratio_
+scree_ax = figure().gca()
+plt.plot(pc_array,scree_vec,'o--')
+scree_ax.set_ylabel('Explained Variance Fraction')
+scree_ax.set_xlabel('Component Number')
